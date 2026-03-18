8f8df3de5d375b8126740e6f405e39859eafd977
yudonglin <506311858@qq.com>
2026-03-18 09:56:23 +0800

    Create 8.ANNOVAR.sh

---
+++ 8.ANNOVAR.sh
@@ -0,0 +1,9 @@
+#https://www.openbioinformatics.org/annovar/annovar_download_form.php
+#自己注册
+cd /data/yudonglin/software/
+
+wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz
+
+
+
+
