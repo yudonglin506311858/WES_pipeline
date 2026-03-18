b83511c11d4166b2cc8e74c3c747338ceb9621bb
yudonglin <506311858@qq.com>
2026-03-18 10:24:39 +0800

    Update 8.ANNOVAR.sh

--- 8.ANNOVAR.sh
+++ 8.ANNOVAR.sh
@@ -1,9 +1,23 @@
 #https://www.openbioinformatics.org/annovar/annovar_download_form.php
 #自己注册
 cd /data/yudonglin/software/
-
 wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz
 tar -zxvf annovar.latest.tar.gz
 
+perl /data/yudonglin/software/annovar/table_annovar.pl
+cd /data/yudonglin/software/annovar
+perl /data/yudonglin/software/annovar/annotate_variation.pl -buildver hg19 -downdb refGene humandb/
+perl /data/yudonglin/software/annovar/annotate_variation.pl -buildver hg19 -downdb gnomad211_exome humandb/
+perl /data/yudonglin/software/annovar/annotate_variation.pl -buildver hg19 -downdb clinvar_20220320 humandb/
 
 
+perl /data/yudonglin/software/annovar/table_annovar.pl \
+filtered.vcf /data/yudonglin/software/annovar/humandb/ \
+-buildver hg19 \
+-out anno \
+-remove \
+-protocol refGene,gnomad211_exome,clinvar_20220320 \
+-operation g,f,f \
+-nastring . \
+-vcfinput \
+-thread 20
